37630dd446889abb7429c4c85c64a748004346b9
zidein07 <zidein07@gmail.com>
2015-08-18 16:42:44 +0400

    view li created

--- vendor/js/code.js
+++ vendor/js/code.js
@@ -1,5 +1,10 @@
-var Person = Backbone.Model.extend({
-	initialize: function(){
-		console.log('Hello from Backbone!');
+LI = Backbone.View.extend({
+	tagName: "li",
+	initialize: function () {
+		$("#ul").append(this.el);
 	}
+});
+
+$(document).ready(function(){
+	var li = new LI();
 });
