49600bec8b547b6f7e34ce10be61e21f7f069412
zidein07 <zidein07@gmail.com>
2015-08-27 15:51:27 +0400

    work with localStorage! add item! Add notify!

--- vendor/js/code.js
+++ vendor/js/code.js
@@ -2,50 +2,55 @@ var app = app || {};
 
 
 $(function() {
-
-app.Model = Backbone.Model.extend({
-  defaults: {
-    title: '',
-    status: 'active',
-    check: false
-  }
-});
-// Модел данных которые будем отправлять в сторедж!
-app.COLLECTION = Backbone.Collection.extend({model: app.Model});
-
-app.VIEW = Backbone.View.extend({
-  template: _.template( $('#template').html() ),
-  initialize: function() {
-    this.render();
-  },
-  render: function() {
-    $('#ul li').remove();
-    this.collection.each(function(col) {
-      this.$el.append(this.template(col.toJSON()))
-    },this);
-  },
-  addItem: function() {
-    if($('#text').val().length > 0) {
-      this.collection.add({ 
-        title: $('#text').val(),
-        cid: this.collection.length
-      },this);
-      $('#text').val('');
-      console.log(collection);
-    } else {
-      $.notify('Вы не можете добавить пустое задание');
+  app.Model = Backbone.Model.extend({
+    defaults: {
+      title: '',
+      status: 'active',
+      check: false
     }
-  }
-});
+  });
+  // Модел данных которые будем отправлять в сторедж!
+  app.COLLECTION = Backbone.Collection.extend({model: app.Model});
 
-collection = new app.COLLECTION();
+  app.VIEW = Backbone.View.extend({
+    template: _.template( $('#template').html() ),
+    initialize: function() {
+      this.render();
+    },
+    render: function() {
+      $('#ul li').remove();
+      this.collection.each(function(col) {
+        this.$el.append(this.template(col.toJSON()))
+      },this);
+    },
+    addItem: function() {
+      if($('#text').val().length > 0) {
+        this.collection.add({ 
+          title: $('#text').val(),
+          id: this.collection.length
+        },this);
+        $.notify('\'' + $('#text').val() + '\'' + ' Добавлено', 'success');
+        console.log('[ ' + $('#text').val() + ' ] успешно добавлено');
+        $('#text').val('');
+        this.addStorage();
+      } else {
+        $.notify('Вы не можете добавить пустое задание');
+      }
+    },
+    addStorage: function() {
+      localStorage.setItem('todo',JSON.stringify(collection));
+    }
+  });
 
-view = new app.VIEW({el: '#ul',collection: collection});
+  collection = new app.COLLECTION();
+
+  view = new app.VIEW({el: '#ul',collection: collection});
 
 
-$('#add').click(function() {
-  view.addItem();
-  view.render();
-});
+  $('#add').click(function() {
+    view.addItem();
+    view.render();
+  });
+
 });
 
