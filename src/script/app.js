85c26016d90953bd673a5ac7e8709e9f429159da
zidein07 <zidein07@gmail.com>
2015-09-23 10:16:51 +0400

    close issue close #13

--- src/script/app.js
+++ src/script/app.js
@@ -3,7 +3,5 @@ var app = app || {};
 $(function () {
   var collection = collection || undefined;
   var view = view || undefined;
-
-  collection = new app.COLLECTION();
   view = new app.VIEW();
 });
