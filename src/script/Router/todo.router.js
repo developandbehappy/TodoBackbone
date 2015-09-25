bea4ee772aabe9145ad410ead45fc7b94e42422f
zidein07 <zidein07@gmail.com>
2015-09-25 10:24:56 +0400

    router add file

--- src/script/Router/todo.router.js
+++ src/script/Router/todo.router.js
@@ -1,5 +1,14 @@
 var app = app || {};
 
-$(function(){
-	console.log('123')
-})
+(function(){
+	app.Router = Backbone.Router.extend({
+		routes: {
+			':hash' : 'action'
+		},
+		action: function (hash) {
+			console.log(hash + ' hello world');
+		}
+	});
+	new app.Router();
+	Backbone.history.start();
+})();
